5f847ae9aa3ff3bf6d99794067295fe53452ba3b
Mona Lisa <YOUR_EMAIL>
2026-04-16 20:58:58 +0700

    ...

---
+++ src/main/java/com/example/t2410e/config/ApplicationConfiguration.java
@@ -0,0 +1,89 @@
+package com.example.t2410e.config;
+
+import com.example.t2410e.entity.User;
+import com.example.t2410e.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
+public class ApplicationConfiguration {
+//    private final AuthenticationProvider authenticationProvider;
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return email->{
+            User user = userRepository.findByEmail(email);
+            if (user == null){
+                throw new UsernameNotFoundException("Email or password is not correct");
+            }
+            return user;
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider =
+                new DaoAuthenticationProvider(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http ) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorizationManagerRequestMatcherRegistry ->
+                                authorizationManagerRequestMatcherRegistry
+                                        .requestMatchers("/api/v1/auth/**").permitAll()
+                                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.addAllowedHeader("*"); // content-type : application/json....1  1   `
+        cfg.addAllowedMethod("*"); // "GET POST PUSH"
+        cfg.addAllowedOrigin("*"); //http://abc.com
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/v1/**", cfg);
+        return source;
+    }
+
+}
--- src/main/java/com/example/t2410e/entity/User.java
+++ src/main/java/com/example/t2410e/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -24,12 +25,12 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return new ArrayList<>();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return getEmail();
     }
 
     @Override
